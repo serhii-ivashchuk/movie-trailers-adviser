2d7b09a50873fbfd1482e587cc1b479e187a8c7c
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-09-10 09:26:10 +0200

    Added Bootstrap class MovieAdvisorApplication

---
+++ src/main/java/MoviesAdvisorApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+public class MoviesAdvisorApplication {
+
+    public static void main(String[] args) {
+    SpringApplication.run(MoviesAdvisorApplication.class, args);
+}
+}
