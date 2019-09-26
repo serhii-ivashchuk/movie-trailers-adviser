b48ff8dead270004fc17f932caaf6ea1dffcf81c
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-09-26 11:59:46 +0200

    Added @Controller to HomeController class

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/HomeController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/HomeController.java
@@ -1,5 +1,9 @@
 package pro.ivashchuk.moviesadvisor.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
-}
+    }
