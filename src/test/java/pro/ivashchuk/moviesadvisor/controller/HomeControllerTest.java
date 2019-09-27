9af57bf7b1f2357af1e1ac6fdc44ff7c299bbd30
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-09-27 22:41:16 +0200

    Added test case

--- src/test/java/pro/ivashchuk/moviesadvisor/controller/HomeControllerTest.java
+++ src/test/java/pro/ivashchuk/moviesadvisor/controller/HomeControllerTest.java
@@ -1,7 +1,21 @@
 package pro.ivashchuk.moviesadvisor.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
 
+
+
+    @Test
+    public void testHomeControllerReturnsHomePageView() {
+
+
+    }
 }
