4690b641b3eaf68bb171323fb5ccd11bd5367045
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-09-27 22:50:30 +0200

    Implemented test case

--- src/test/java/pro/ivashchuk/moviesadvisor/controller/HomeControllerTest.java
+++ src/test/java/pro/ivashchuk/moviesadvisor/controller/HomeControllerTest.java
@@ -2,20 +2,28 @@ package pro.ivashchuk.moviesadvisor.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
 
-
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    public void testHomeControllerReturnsHomePageView() {
-
-
+    public void testHomeControllerReturnsHomePageView() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to the Movie Advisor App")));
     }
 }
