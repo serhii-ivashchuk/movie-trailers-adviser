4d4a5d4148fcc103ac67cfa029dfd24406bd9ec8
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-01 12:00:46 +0200

    Added class-level @RequestMapping annotation

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/MovieController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/MovieController.java
@@ -1,7 +1,11 @@
 package pro.ivashchuk.moviesadvisor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/movies")
 public class MovieController {
+
 }
