e72a47e707076dcd685dec684f03dee1352ee453
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-01 18:44:27 +0200

    Added getAllUsers() to UserController

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/UserController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/UserController.java
@@ -1,4 +1,15 @@
 package pro.ivashchuk.moviesadvisor.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping
+    public String getAllUsers() {
+        return "All_Users";
+    }
 }
