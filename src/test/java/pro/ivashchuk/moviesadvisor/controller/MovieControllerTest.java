6e0e575e81d569e4f060c7d3af44b95f73c6e80d
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-01 18:29:24 +0200

    Added test case to MovieControllerTest

--- src/test/java/pro/ivashchuk/moviesadvisor/controller/MovieControllerTest.java
+++ src/test/java/pro/ivashchuk/moviesadvisor/controller/MovieControllerTest.java
@@ -1,7 +1,29 @@
 package pro.ivashchuk.moviesadvisor.controller;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MovieController.class)
 public class MovieControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testMovieCotntrollerReturnsAllMoviesPageView() throws Exception{
+        mockMvc.perform(get("/movies"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("All_Movies"))
+                .andExpect(content().string(containsString("All Movies")));
+    }
 }
