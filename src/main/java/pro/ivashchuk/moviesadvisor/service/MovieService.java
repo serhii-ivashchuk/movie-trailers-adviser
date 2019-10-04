6c440bb0d1db4c59195b43a8d5baa276a778aa4c
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-04 18:05:05 +0200

    Added getAllMovies() to MovieService

--- src/main/java/pro/ivashchuk/moviesadvisor/service/MovieService.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/service/MovieService.java
@@ -1,4 +1,10 @@
 package pro.ivashchuk.moviesadvisor.service;
 
+import pro.ivashchuk.moviesadvisor.domain.Movie;
+
+import java.util.List;
+
 public interface MovieService {
+
+    List<Movie> getAllMovies();
 }
