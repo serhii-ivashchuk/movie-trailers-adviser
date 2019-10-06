7e1d042bcf152ad1ed406d0f3c9ee4ef98521132
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-06 21:26:09 +0200

    Implemented methods of UserService in UserServiceImpl

--- src/main/java/pro/ivashchuk/moviesadvisor/service/UserServiceImpl.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/service/UserServiceImpl.java
@@ -1,4 +1,34 @@
 package pro.ivashchuk.moviesadvisor.service;
 
-public class UserServiceImpl {
+import pro.ivashchuk.moviesadvisor.domain.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+    @Override
+    public void deleteUser(User user) {
+
+    }
 }
+
