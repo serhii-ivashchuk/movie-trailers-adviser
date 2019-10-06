3c4bbfb39210eaec2de590dba7ca6dadabf98020
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-06 21:27:11 +0200

    Implemented methods of MovieService in MovieServiceImpl

--- src/main/java/pro/ivashchuk/moviesadvisor/service/MovieServiceImpl.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/service/MovieServiceImpl.java
@@ -1,4 +1,32 @@
 package pro.ivashchuk.moviesadvisor.service;
 
-public class MovieServiceImpl {
+import pro.ivashchuk.moviesadvisor.domain.Movie;
+
+import java.util.List;
+
+public class MovieServiceImpl implements MovieService {
+    @Override
+    public List<Movie> getAllMovies() {
+        return null;
+    }
+
+    @Override
+    public Movie getMovieById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void addMovie(Movie movie) {
+
+    }
+
+    @Override
+    public void updateMovie(Movie movie) {
+
+    }
+
+    @Override
+    public void deleteMovie(Movie movie) {
+
+    }
 }
