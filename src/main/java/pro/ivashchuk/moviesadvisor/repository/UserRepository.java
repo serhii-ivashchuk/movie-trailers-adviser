14939cbf30da404004010f3e854077788739ea88
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-09 14:05:35 +0200

    Added JpaRepository to UserRepository

--- src/main/java/pro/ivashchuk/moviesadvisor/repository/UserRepository.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package pro.ivashchuk.moviesadvisor.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pro.ivashchuk.moviesadvisor.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
