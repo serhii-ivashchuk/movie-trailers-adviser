ac8ffbd478d832c589dc0e66a548d2627aed3086
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-09 17:08:08 +0200

    Added addNewMovie() to MovieController

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/MovieController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/MovieController.java
@@ -1,8 +1,11 @@
 package pro.ivashchuk.moviesadvisor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pro.ivashchuk.moviesadvisor.domain.Movie;
 
 @Controller
 @RequestMapping("/movies")
@@ -11,5 +14,11 @@ public class MovieController {
     @GetMapping
     public String getAllMovies(){
         return "All_Movies";
+    }
+
+    @GetMapping("/addNewMovie")
+    public String addNewMovie(Model model) {
+        model.addAttribute(new Movie());
+        return "Add_New_Movie";
     }
 }
