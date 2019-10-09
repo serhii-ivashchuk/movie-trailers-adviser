09c9ee27bddf32a18af32a1b6148d01f4468659e
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-09 13:09:38 +0200

    Added getter and setters to User

--- src/main/java/pro/ivashchuk/moviesadvisor/domain/User.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/domain/User.java
@@ -2,6 +2,23 @@ package pro.ivashchuk.moviesadvisor.domain;
 
 public class User {
     private Long id;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     private String username;
     private String password;
 }
