458a6bb34128c2634e9fcc225977c6aac6979ef0
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-09 13:12:53 +0200

    Added constructor with parameters to User

--- src/main/java/pro/ivashchuk/moviesadvisor/domain/User.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/domain/User.java
@@ -10,6 +10,11 @@ public class User {
     public User() {
     }
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
