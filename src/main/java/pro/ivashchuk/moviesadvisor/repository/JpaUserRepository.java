3d589269acb57615dbce64a7aa5ff0db60e6ec81
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-09 14:07:09 +0200

    Refactored name of UserRepository to JpaUserRepository

--- src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaUserRepository.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaUserRepository.java
@@ -3,5 +3,5 @@ package pro.ivashchuk.moviesadvisor.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.ivashchuk.moviesadvisor.domain.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface JpaUserRepository extends JpaRepository<User, Long> {
 }
