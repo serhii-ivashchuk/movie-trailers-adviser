989f22365673fe82d9383b5b6fbbef2f6de3e45e
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-09 14:19:56 +0200

    Added initial data that would populate DB to data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+delete from Movie
+delete from User
+
+insert into Movie (id, movieName, ranking)
+values ('', 'Terminator', '10');
+insert into Movie (id, movieName, ranking)
+values ('', 'The Shawshank Redemption (1994)', '10');
+insert into Movie (id, movieName, ranking)
+values ('', 'Inception (2010)', '10');
+insert into Movie (id, movieName, ranking)
+values ('', 'Fight Club (1999)', '10');
+insert into Movie (id, movieName, ranking)
+values ('', 'Pulp Fiction (1994)', '10');
+insert into Movie (id, movieName, ranking)
+values ('', 'Forrest Gump (1994)', '10');
+insert into Movie (id, movieName, ranking)
+values ('', 'The Lord of the Rings: The Fellowship of the Ring (2001)', '10');
+insert into Movie (id, movieName, ranking)
+values ('', 'The Matrix (1999)', '10');
+insert into Movie (id, movieName, ranking)
+values ('', 'The Godfather (1972)', '10');
+
+
+
+insert into User (id, username, password)
+values ('', 'Serhii Ivashchuk', 'password');
+insert into User (id, username, password)
+values ('', 'Robert Martin', 'password');
