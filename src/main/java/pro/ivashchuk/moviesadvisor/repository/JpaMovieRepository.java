2b01278d0be0454ed9b59f4814152384f04a18c2
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-14 14:19:52 +0200

    Added save() to JpaMovieRepository

--- src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaMovieRepository.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaMovieRepository.java
@@ -3,9 +3,11 @@ package pro.ivashchuk.moviesadvisor.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.ivashchuk.moviesadvisor.domain.Movie;
 
+import javax.persistence.Id;
 import java.util.List;
 
 public interface JpaMovieRepository extends JpaRepository<Movie, Long> {
 
     List<Movie> findAll();
+    Movie save(Movie movie);
 }
