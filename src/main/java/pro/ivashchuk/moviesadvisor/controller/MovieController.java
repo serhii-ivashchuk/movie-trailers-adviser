1c937b66f31264b1c06b6dc2e6af3d869e225e0d
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-14 12:47:47 +0200

    Added BL to processNewMovie() in MovieController

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/MovieController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/MovieController.java
@@ -39,6 +39,7 @@ public class MovieController {
 
     @PostMapping("/addNewMovie")
     public String processNewMovie(@Valid Movie movie) {
+        jpaMovieRepository.save(movie);
         return "redirect:/";
     }
 }
