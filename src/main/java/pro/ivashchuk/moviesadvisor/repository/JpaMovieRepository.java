319a365e50ae834ffa3c714bee6881153ceda972
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-14 14:20:30 +0200

    Added findById to JpaMovieRepository

--- src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaMovieRepository.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaMovieRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 public interface JpaMovieRepository extends JpaRepository<Movie, Long> {
 
     List<Movie> findAll();
+    Movie findById(Id id);
     Movie save(Movie movie);
 }
