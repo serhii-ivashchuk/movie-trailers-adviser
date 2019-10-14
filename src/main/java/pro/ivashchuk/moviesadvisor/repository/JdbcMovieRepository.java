6d1bb5b2200c4c3d5edec9919c6ce1a31e4341ab
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-14 16:55:00 +0200

    Created JdbcMovieRepository with implementation of MovieRepository

---
+++ src/main/java/pro/ivashchuk/moviesadvisor/repository/JdbcMovieRepository.java
@@ -0,0 +1,21 @@
+package pro.ivashchuk.moviesadvisor.repository;
+
+import pro.ivashchuk.moviesadvisor.domain.Movie;
+
+public class JdbcMovieRepository implements MovieRepository {
+
+    @Override
+    public Iterable<Movie> findAll() {
+        return null;
+    }
+
+    @Override
+    public Movie findById(String id) {
+        return null;
+    }
+
+    @Override
+    public Movie save(Movie movie) {
+        return null;
+    }
+}
