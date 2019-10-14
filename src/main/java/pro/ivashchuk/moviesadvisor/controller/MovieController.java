833dded32b23c10f4ad31aa5cf55267743ff8f9d
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-14 12:46:21 +0200

    Added simple BL to getAllMovies() in MovieController

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/MovieController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/MovieController.java
@@ -6,17 +6,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pro.ivashchuk.moviesadvisor.domain.Movie;
+import pro.ivashchuk.moviesadvisor.repository.JpaMovieRepository;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/movies")
 public class MovieController {
 
+    private JpaMovieRepository jpaMovieRepository;
+
+    public MovieController(JpaMovieRepository jpaMovieRepository) {
+        this.jpaMovieRepository = jpaMovieRepository;
+    }
+
+
     @GetMapping
-    public String getAllMovies(){
+    public String getAllMovies(Model model) {
+        List<Movie> movies = new ArrayList<>(jpaMovieRepository.findAll());
+        model.addAttribute("movies", movies);
         return "All_Movies";
     }
+
 
     @GetMapping("/addNewMovie")
     public String addNewMovie(Model model) {
@@ -25,7 +38,7 @@ public class MovieController {
     }
 
     @PostMapping("/addNewMovie")
-    public String processNewMovie (@Valid Movie movie) {
+    public String processNewMovie(@Valid Movie movie) {
         return "redirect:/";
     }
 }
