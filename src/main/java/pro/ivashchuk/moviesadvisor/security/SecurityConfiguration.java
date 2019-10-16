4eda64f3d785cf6feda37b1f04058855ef65cff8
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-16 16:30:23 +0200

    Added @Configuration to SecurityConfiguration

--- src/main/java/pro/ivashchuk/moviesadvisor/security/SecurityConfiguration.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/security/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package pro.ivashchuk.moviesadvisor.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 }
