939c17bb76343f07e85f5c77db5f670e63c86888
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-17 14:29:49 +0200

    Added UserAuthorities table schema to schema-h2.sql

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -12,3 +12,9 @@
   enabled boolean not null,
   PRIMARY KEY (username)
 );
+
+ create table if not exists UserAuthorities (
+ username VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users(username)
+ );
