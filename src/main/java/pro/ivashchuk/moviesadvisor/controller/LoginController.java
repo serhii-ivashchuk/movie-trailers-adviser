fb56975cf01d41ea54eceeace3d0b13e3d2fe38d
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-24 20:57:40 +0200

    Added returnLoginPage() to LoginController

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/LoginController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/LoginController.java
@@ -1,7 +1,14 @@
 package pro.ivashchuk.moviesadvisor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+
+    @RequestMapping("/login")
+    public String returnLoginPage(){
+        System.out.println("returnLoginPage() of LoginController is invoked");
+        return "login";
+    }
 }
