c32fdc3cb5a66c373e17a5e4376e7ea6f5d92549
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-10-26 21:53:35 +0200

    Added Logger to LoginController

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/LoginController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/LoginController.java
@@ -1,14 +1,18 @@
 package pro.ivashchuk.moviesadvisor.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 
+    final static Logger log = LoggerFactory.getLogger(LoginController.class);
+
     @RequestMapping("/login")
     public String returnLoginPage(){
-        System.out.println("returnLoginPage() of LoginController is invoked");
+        log.info("returnLoginPage() of LoginController is invoked");
         return "login";
     }
 }
