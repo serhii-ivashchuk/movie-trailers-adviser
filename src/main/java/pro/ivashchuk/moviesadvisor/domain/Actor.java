d7fe1d069b82a71577ab1d7ec8f6ad271cc503d9
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-05 11:14:44 +0100

    Added getters and setters to Actor entity

--- src/main/java/pro/ivashchuk/moviesadvisor/domain/Actor.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/domain/Actor.java
@@ -3,4 +3,20 @@ package pro.ivashchuk.moviesadvisor.domain;
 public class Actor {
     private String name;
     private String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
