4e4cb79fcd452898c9ab23551de728c5d6c0793d
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-09 21:39:40 +0100

    Added Logger to ActorController

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/ActorController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/ActorController.java
@@ -1,9 +1,13 @@
 package pro.ivashchuk.moviesadvisor.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/actors")
 public class ActorController {
+
+    final static Logger log = LoggerFactory.getLogger(ActorController.class);
 }
