b5eb81ac171b2c5d1f0d17fcbb5fbe2c31446405
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-09 21:38:23 +0100

    Added @RequestMapping("/actors") to ActorController

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/ActorController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/ActorController.java
@@ -1,7 +1,9 @@
 package pro.ivashchuk.moviesadvisor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/actors")
 public class ActorController {
 }
