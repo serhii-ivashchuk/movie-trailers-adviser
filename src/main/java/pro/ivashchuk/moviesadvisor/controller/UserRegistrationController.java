10a36fcf4dda13983e9d2bc1eae70766fce11347
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-11 11:27:27 +0100

    Added PasswordEncoder field

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/UserRegistrationController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/UserRegistrationController.java
@@ -1,12 +1,17 @@
 package pro.ivashchuk.moviesadvisor.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import pro.ivashchuk.moviesadvisor.domain.User;
-import pro.ivashchuk.moviesadvisor.repository.JpaUserRepository;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
+        import org.springframework.security.crypto.password.PasswordEncoder;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.PostMapping;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import pro.ivashchuk.moviesadvisor.repository.JpaUserRepository;
+        import pro.ivashchuk.moviesadvisor.security.UserRegistrationForm;
+
+        import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
@@ -15,10 +20,12 @@ public class UserRegistrationController {
     final static Logger log = LoggerFactory.getLogger(UserRegistrationController.class);
 
     private JpaUserRepository jpaUserRepository;
+    private PasswordEncoder passwordEncoder;
 
 
-    public UserRegistrationController(JpaUserRepository jpaUserRepository) {
+    public UserRegistrationController(JpaUserRepository jpaUserRepository, PasswordEncoder passwordEncoder) {
         this.jpaUserRepository = jpaUserRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/registration")
