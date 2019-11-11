40abdc1514972642b7e77b8ac72be27bd643da5f
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-11 11:35:41 +0100

    Added Logger

--- src/main/java/pro/ivashchuk/moviesadvisor/security/UserRegistrationForm.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/security/UserRegistrationForm.java
@@ -2,6 +2,8 @@ package pro.ivashchuk.moviesadvisor.security;
 
 public class UserRegistrationForm {
 
+    final static Logger log = LoggerFactory.getLogger(UserRegistrationForm.class);
+
     private String username;
     private String password;
 }
