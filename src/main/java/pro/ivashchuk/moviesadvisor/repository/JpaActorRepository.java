b2c8e26f6bc97e16f47563f6d52f73cc68358df3
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-11 10:24:27 +0100

    Added save()

--- src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaActorRepository.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaActorRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface JpaActorRepository extends JpaRepository<Actor, Long> {
     List<Actor> findAll();
     Optional<Actor> findById(Long id);
+    Actor save(Actor actor);
 }
