670f36a356672e9bf49be50b71a1d5f4abbdaaa3
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-11 11:41:58 +0100

    Added getter and setters

--- src/main/java/pro/ivashchuk/moviesadvisor/security/UserRegistrationForm.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/security/UserRegistrationForm.java
@@ -10,4 +10,28 @@ public class UserRegistrationForm {
     private String username;
     private String password;
     private String phrase;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhrase() {
+        return phrase;
+    }
+
+    public void setPhrase(String phrase) {
+        this.phrase = phrase;
+    }
 }
