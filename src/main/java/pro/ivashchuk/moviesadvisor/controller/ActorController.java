475c8d34316598e02df13ce3945cd3c5448ca7c5
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-11 11:14:15 +0100

    Added jpaActorRepository field

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/ActorController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/ActorController.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pro.ivashchuk.moviesadvisor.repository.JpaActorRepository;
 
 @Controller
 @RequestMapping("/actors")
 public class ActorController {
 
     final static Logger log = LoggerFactory.getLogger(ActorController.class);
+
+    private JpaActorRepository jpaActorRepository;
 }
