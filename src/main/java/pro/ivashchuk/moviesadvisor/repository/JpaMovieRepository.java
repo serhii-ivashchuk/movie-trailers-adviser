29335efb0aa5774fe147f0334f6ceed9fd53009f
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-11 10:13:23 +0100

    Changed findById, now it returns Optional<Movie>

--- src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaMovieRepository.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaMovieRepository.java
@@ -3,13 +3,13 @@ package pro.ivashchuk.moviesadvisor.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.ivashchuk.moviesadvisor.domain.Movie;
 
-import javax.persistence.Id;
 import java.util.List;
+import java.util.Optional;
 
 public interface JpaMovieRepository extends JpaRepository<Movie, Long> {
 
     List<Movie> findAll();
-    Movie findById(Id id);
+    Optional<Movie> findById(Long id);
     Movie save(Movie movie);
     void delete(Movie movie);
 }
