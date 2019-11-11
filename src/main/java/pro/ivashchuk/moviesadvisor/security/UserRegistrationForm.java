c743b6b9d1ec63a87ceb8136de496ccb574e1e26
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-11 11:39:30 +0100

    Added logger necessary imports

--- src/main/java/pro/ivashchuk/moviesadvisor/security/UserRegistrationForm.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/security/UserRegistrationForm.java
@@ -1,5 +1,8 @@
 package pro.ivashchuk.moviesadvisor.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class UserRegistrationForm {
 
     final static Logger log = LoggerFactory.getLogger(UserRegistrationForm.class);
