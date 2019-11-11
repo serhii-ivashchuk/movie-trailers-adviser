0c56d2c6dd5d5baf56539c92173e03808352c960
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-11 19:12:47 +0100

    Added JpaUserRepository field to ActorController()

--- src/main/java/pro/ivashchuk/moviesadvisor/controller/ActorController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/controller/ActorController.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pro.ivashchuk.moviesadvisor.domain.Actor;
 import pro.ivashchuk.moviesadvisor.repository.JpaActorRepository;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,11 +22,32 @@ public class ActorController {
 
     private JpaActorRepository jpaActorRepository;
 
+    public ActorController(JpaActorRepository jpaActorRepository) {
+        this.jpaActorRepository = jpaActorRepository;
+    }
+
     @GetMapping
     public String getAllActors(Model model) {
         log.info("getAllActors() of ActorController is invoked");
         List<Actor> actors = new ArrayList<>(jpaActorRepository.findAll());
         model.addAttribute("actors", actors);
-        return "All_actors";}
+        return "All_actors";
+    }
+
+    @GetMapping("/addNewActor")
+    public String addNewActor(Model model) {
+        log.info("addNewActor() of ActorController is invoked");
+        model.addAttribute("actor", new Actor());
+        return "Add_New_Actor";
+    }
+
+    @PostMapping("/addNewActor")
+    public String processNewActor(@Valid Actor actor) {
+        log.info("processNewActor() of ActorController is invoked");
+        jpaActorRepository.save(actor);
+        log.info("processNewActor() > jpaActorRepository.save() is invoked");
+        log.info("Actor: "+actor);
+        return "redirect:/actors";
+    }
 
 }
