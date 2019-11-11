f250e1366f568fbc2a1d1307edd465b3980b2899
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-11 10:23:28 +0100

    Added findAll()

--- src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaActorRepository.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaActorRepository.java
@@ -3,5 +3,9 @@ package pro.ivashchuk.moviesadvisor.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.ivashchuk.moviesadvisor.domain.Actor;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface JpaActorRepository extends JpaRepository<Actor, Long> {
+    List<Actor> findAll();
 }
