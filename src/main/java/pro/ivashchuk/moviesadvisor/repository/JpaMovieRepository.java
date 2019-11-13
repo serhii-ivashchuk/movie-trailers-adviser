316810eabd067ec76f105fefe4d62cbf52f507f4
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-13 09:39:52 +0100

    Reformatted code

--- src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaMovieRepository.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaMovieRepository.java
@@ -7,9 +7,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface JpaMovieRepository extends JpaRepository<Movie, Long> {
-
     List<Movie> findAll();
+
     Optional<Movie> findById(Long id);
+
     Movie save(Movie movie);
+
     void delete(Movie movie);
 }
