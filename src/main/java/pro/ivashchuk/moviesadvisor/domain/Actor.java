06b874059a1d3944730a7f1a6a26d47ab034e5b2
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-13 09:27:05 +0100

    Reformatted code

--- src/main/java/pro/ivashchuk/moviesadvisor/domain/Actor.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/domain/Actor.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Actor {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
@@ -37,6 +37,6 @@ public class Actor {
     @Override
     public String toString() {
         return String.format(
-                "Actor id[id=%d, name='%s', surname='%s'" , id, name, surname);
+                "Actor id[id=%d, name='%s', surname='%s'", id, name, surname);
     }
 }
