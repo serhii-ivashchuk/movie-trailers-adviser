1ad6bd123802e0333c47071df2bc650d43192def
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-13 09:47:07 +0100

    Changed findById() from Id id on Long id returning Optional<User>

--- src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaUserRepository.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaUserRepository.java
@@ -3,12 +3,14 @@ package pro.ivashchuk.moviesadvisor.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.ivashchuk.moviesadvisor.domain.User;
 
-import javax.persistence.Id;
 import java.util.List;
+import java.util.Optional;
 
 public interface JpaUserRepository extends JpaRepository<User, Long> {
     List<User> findAll();
-    User findById(Id id);
+
+    Optional<User> findById(Long id);
+
     User findByUsername(String username);
 
     User save(User user);
