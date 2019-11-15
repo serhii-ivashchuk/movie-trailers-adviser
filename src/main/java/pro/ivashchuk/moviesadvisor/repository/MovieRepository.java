3116dfcb52cda61b3fbf63a309e87036a048717d
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-15 14:32:26 +0100

    Deleted MovieRepository as unnecessary (JpaMovieRepository is implemented)

--- src/main/java/pro/ivashchuk/moviesadvisor/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package pro.ivashchuk.moviesadvisor.repository;
-
-import pro.ivashchuk.moviesadvisor.domain.Movie;
-
-public interface MovieRepository {
-    Iterable<Movie> findAll();
-    Movie findById(String id);
-    Movie save(Movie movie);
-}
