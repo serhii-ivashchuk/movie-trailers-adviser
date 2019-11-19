93c020d9250a67461f538ded49cb0eea0e16b7bd
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-19 20:14:11 +0100

    Added addNewUser()

--- src/main/java/pro/ivashchuk/moviesadvisor/api/rest/UserController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/api/rest/UserController.java
@@ -1,7 +1,31 @@
 package pro.ivashchuk.moviesadvisor.api.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.ivashchuk.moviesadvisor.domain.User;
+import pro.ivashchuk.moviesadvisor.repository.JpaUserRepository;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(path="/api/users", produces = "application/json")
+@CrossOrigin(origins = "*")
 public class UserController {
+    private JpaUserRepository jpaUserRepository;
+
+    public UserController(JpaUserRepository jpaUserRepository) {
+        this.jpaUserRepository = jpaUserRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+       return jpaUserRepository.findAll();
+    }
+
+    @GetMapping("/registration")
+    public String addNewUser() {
+        return "";
+    }
 }
