2f23ab4496e913683615509f752654ea7fb31012
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-27 20:44:52 +0100

    Added implements UserDetails

--- src/main/java/pro/ivashchuk/moviesadvisor/security/UserRepositoryUserDetailsService.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/security/UserRepositoryUserDetailsService.java
@@ -1,7 +1,8 @@
 package pro.ivashchuk.moviesadvisor.security;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserRepositoryUserDetailsService {
+public class UserRepositoryUserDetailsService implements UserDetails {
 }
