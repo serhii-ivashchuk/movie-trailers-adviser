1700ea5b53cafe5414fbe297176dcc9bcc085276
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-28 20:14:43 +0100

    Added JpaUserRepository jpaUserRepository

--- src/main/java/pro/ivashchuk/moviesadvisor/security/UserRepositoryUserDetailsService.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/security/UserRepositoryUserDetailsService.java
@@ -2,7 +2,10 @@ package pro.ivashchuk.moviesadvisor.security;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import pro.ivashchuk.moviesadvisor.repository.JpaUserRepository;
 
 @Service
 public class UserRepositoryUserDetailsService implements UserDetails {
+
+    JpaUserRepository jpaUserRepository;
 }
