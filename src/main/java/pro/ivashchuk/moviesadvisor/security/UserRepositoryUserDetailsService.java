6b8c56bda7cd9650f58ff78730e7954361c4f0d1
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-28 20:16:19 +0100

    Added @Autowired

--- src/main/java/pro/ivashchuk/moviesadvisor/security/UserRepositoryUserDetailsService.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/security/UserRepositoryUserDetailsService.java
@@ -1,5 +1,6 @@
 package pro.ivashchuk.moviesadvisor.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import pro.ivashchuk.moviesadvisor.repository.JpaUserRepository;
@@ -9,6 +10,7 @@ public class UserRepositoryUserDetailsService implements UserDetails {
 
     JpaUserRepository jpaUserRepository;
 
+    @Autowired
     public UserRepositoryUserDetailsService userRepositoryUserDetailsService(JpaUserRepository jpaUserRepository) {
         this.jpaUserRepository = jpaUserRepository;
     }
