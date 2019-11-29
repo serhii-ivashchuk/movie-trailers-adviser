b80d864c48ec9eb42110c775f843807c9aa11a80
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-29 08:19:48 +0100

    Added loadUserByUsername()

--- src/main/java/pro/ivashchuk/moviesadvisor/security/UserRepositoryUserDetailsService.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/security/UserRepositoryUserDetailsService.java
@@ -1,6 +1,7 @@
 package pro.ivashchuk.moviesadvisor.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import pro.ivashchuk.moviesadvisor.repository.JpaUserRepository;
@@ -14,4 +15,9 @@ public class UserRepositoryUserDetailsService implements UserDetailsService {
     public UserRepositoryUserDetailsService userRepositoryUserDetailsService(JpaUserRepository jpaUserRepository) {
         this.jpaUserRepository = jpaUserRepository;
     }
+
+
+    public UserDetails loadUserByUsername(String username) {
+    }
+
 }
