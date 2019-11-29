34fed6188612c2cbe0e11f58f4f47e315642f74d
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-29 08:41:21 +0100

    Added import for @EnabledWebSecurity

--- src/main/java/pro/ivashchuk/moviesadvisor/security/SecurityConfiguration.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package pro.ivashchuk.moviesadvisor.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
