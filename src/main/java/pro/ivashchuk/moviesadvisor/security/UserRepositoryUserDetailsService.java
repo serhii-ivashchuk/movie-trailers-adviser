79de7c9ad01b9aced0290dacf6494c1fdd46727b
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-29 08:16:37 +0100

    Added UserRepositoryUserDetailsService implements UserDetailsService

--- src/main/java/pro/ivashchuk/moviesadvisor/security/UserRepositoryUserDetailsService.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/security/UserRepositoryUserDetailsService.java
@@ -1,12 +1,12 @@
 package pro.ivashchuk.moviesadvisor.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import pro.ivashchuk.moviesadvisor.repository.JpaUserRepository;
 
 @Service
-public class UserRepositoryUserDetailsService implements UserDetails {
+public class UserRepositoryUserDetailsService implements UserDetailsService {
 
     JpaUserRepository jpaUserRepository;
 
