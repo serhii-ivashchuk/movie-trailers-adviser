8f9eff6f8422ce39195188326b953f0cecd6939e
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-11-30 21:15:08 +0100

    Added @Override to configure(HttpSecurity http)

--- src/main/java/pro/ivashchuk/moviesadvisor/security/SecurityConfiguration.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/security/SecurityConfiguration.java
@@ -32,6 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(encoder());
     }
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
     }
 }
