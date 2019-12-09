a0e5dff828ecdaca6fc0f4313cea88eae1b40ab6
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-12-09 16:36:28 +0100

    Added @RequestMapping

--- src/main/java/pro/ivashchuk/moviesadvisor/api/rest/ActorController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/api/rest/ActorController.java
@@ -1,7 +1,9 @@
 package pro.ivashchuk.moviesadvisor.api.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path="/api/actors", produces = "application/json")
 public class ActorController {
 }
