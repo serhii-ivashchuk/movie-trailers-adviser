507da12d6292da2ac70d72409e1597bc74689fdd
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-12-10 10:06:22 +0100

    Added JpaActorRepository field

--- src/main/java/pro/ivashchuk/moviesadvisor/api/rest/ActorController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/api/rest/ActorController.java
@@ -2,8 +2,10 @@ package pro.ivashchuk.moviesadvisor.api.rest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.ivashchuk.moviesadvisor.repository.JpaActorRepository;
 
 @RestController
 @RequestMapping(path="/api/actors", produces = "application/json")
 public class ActorController {
+    private JpaActorRepository jpaActorRepository;
 }
