06517972ac3bf452b407453eeced34c4e6127e96
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-12-11 08:02:13 +0100

    Add JpaMovieRepository field

--- src/main/java/pro/ivashchuk/moviesadvisor/api/rest/MovieController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/api/rest/MovieController.java
@@ -2,8 +2,10 @@ package pro.ivashchuk.moviesadvisor.api.rest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.ivashchuk.moviesadvisor.repository.JpaMovieRepository;
 
 @RestController
 @RequestMapping(path="/api/movies", produces = "application/json")
 public class MovieController {
+    private JpaMovieRepository jpaMovieRepository;
 }
