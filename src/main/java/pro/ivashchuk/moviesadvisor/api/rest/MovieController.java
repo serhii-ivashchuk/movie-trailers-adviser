7b1fa7eb77b0512b3e754adfdd71da1a442382ae
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-12-14 22:18:11 +0100

    Added getAllMovies()

--- src/main/java/pro/ivashchuk/moviesadvisor/api/rest/MovieController.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/api/rest/MovieController.java
@@ -1,15 +1,26 @@
 package pro.ivashchuk.moviesadvisor.api.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.ivashchuk.moviesadvisor.domain.Movie;
+import pro.ivashchuk.moviesadvisor.domain.User;
 import pro.ivashchuk.moviesadvisor.repository.JpaMovieRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/api/movies", produces = "application/json")
 public class MovieController {
+
     private JpaMovieRepository jpaMovieRepository;
 
     public MovieController(JpaMovieRepository jpaMovieRepository) {
         this.jpaMovieRepository = jpaMovieRepository;
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies(){
+        return jpaMovieRepository.findAll();
     }
 }
