5a3a55c425cfb48899bb20093402e598a6e5581b
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-12-26 22:02:52 +0100

    Add @Repository

--- src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaActorRepository.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/repository/JpaActorRepository.java
@@ -1,11 +1,13 @@
 package pro.ivashchuk.moviesadvisor.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pro.ivashchuk.moviesadvisor.domain.Actor;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository("actorRepository")
 public interface JpaActorRepository extends JpaRepository<Actor, Long> {
     List<Actor> findAll();
 
