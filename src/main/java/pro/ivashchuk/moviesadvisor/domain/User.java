72d804449d241a34337981c7ae88a2f8ed44c5ba
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-12-28 19:51:06 +0100

    Add Set<Role> roles field

--- src/main/java/pro/ivashchuk/moviesadvisor/domain/User.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/domain/User.java
@@ -60,6 +60,17 @@ public class User implements UserDetails {
         this.phrase = phrase;
     }
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 
     @ManyToMany(mappedBy = "movieUserCommitters")
     Set<Movie> committedMovies;
