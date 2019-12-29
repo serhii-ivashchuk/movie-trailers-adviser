eeb5fab455fdc2ba9e0a1e9f4d0f9e5833c7ba79
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2019-12-29 23:12:03 +0100

    Add shouldGenerateId

--- src/test/java/pro/ivashchuk/moviesadvisor/domain/ActorTest.java
+++ src/test/java/pro/ivashchuk/moviesadvisor/domain/ActorTest.java
@@ -1,5 +1,31 @@
 package pro.ivashchuk.moviesadvisor.domain;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class ActorTest {
 
+    final static Logger log = LoggerFactory.getLogger(MovieTest.class);
+
+    @Autowired
+    TestEntityManager testEntityManager;
+
+    Actor actor = new Actor("Tom", "Hanks", "Tom Hanks.jpg");
+
+    @Test
+    public void shouldGenerateId() {
+        testEntityManager.persistFlushFind(actor);
+        assertNotNull(actor.getId());
+    }
 }
