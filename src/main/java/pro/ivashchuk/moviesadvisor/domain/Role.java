51b05deb7ce9bdb031ce73226af4392b284c56c5
Serhii-Ivashchuk <serhii.ivashchuk@gmail.com>
2020-01-06 22:03:30 +0100

    Add getAuthority()

--- src/main/java/pro/ivashchuk/moviesadvisor/domain/Role.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/domain/Role.java
@@ -1,11 +1,13 @@
 package pro.ivashchuk.moviesadvisor.domain;
 
 
+import org.springframework.security.core.GrantedAuthority;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -32,5 +34,10 @@ public class Role {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getRole();
     }
 }
