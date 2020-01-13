cf568c5e5870b480ae833f199d71ec4c8eeb8129
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-01-13 13:16:28 +0100

    Added getter and setter for Set<User> users

--- src/main/java/pro/ivashchuk/moviesadvisor/domain/Role.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/domain/Role.java
@@ -45,6 +45,14 @@ public class Role implements GrantedAuthority {
         this.role = role;
     }
 
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
     @Override
     public String getAuthority() {
         return getRole();
