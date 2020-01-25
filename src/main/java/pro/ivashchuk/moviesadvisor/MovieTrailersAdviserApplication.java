5fe26ef2777b6b940251704fe698613c30dde68f
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-01-25 10:58:28 +0100

    Renamed to MovieTrailersAdviserApplication

--- src/main/java/pro/ivashchuk/moviesadvisor/MovieTrailersAdviserApplication.java
+++ src/main/java/pro/ivashchuk/moviesadvisor/MovieTrailersAdviserApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
-public class MoviesAdvisorApplication {
+public class MovieTrailersAdviserApplication {
 
     public static void main(String[] args) {
-    SpringApplication.run(MoviesAdvisorApplication.class, args);
+    SpringApplication.run(MovieTrailersAdviserApplication.class, args);
 }
 }
